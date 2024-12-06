5b991980db541d5fb70f764dd90eac455dcddbe7
Nihal Singh <142614932+Emperorns@users.noreply.github.com>
2024-12-06 06:37:21 +0530

    Update info.py

--- info.py
+++ info.py
@@ -59,7 +59,7 @@ DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in enviro
 
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Selfmixbot:nehalsingh969797@cluster0.kb5xjos.mongodb.net/?retryWrites=true&w=majority")   # IF Multiple Database Is False Then Fill Only This Database Url.
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb://nehal969797:nehalsingh969797@cluster0/?ssl=true&replicaSet=atlas-n29pii-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0")   # IF Multiple Database Is False Then Fill Only This Database Url.
 DATABASE_NAME = environ.get('DATABASE_NAME', "techvjclonefilterbot")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'vjcollection')
 
