1cbcbba2f2b4ef09eaf085732a1ff0756f433375
Nihal Singh <142614932+Emperorns@users.noreply.github.com>
2025-03-28 22:06:52 +0530

    Update info.py

--- info.py
+++ info.py
@@ -63,12 +63,12 @@ DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://nehal969797:nehalsingh
 DATABASE_NAME = environ.get('DATABASE_NAME', "techvjclonefilterbot")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'vjcollection')
 
-MULTIPLE_DATABASE = bool(environ.get('MULTIPLE_DATABASE', False)) # Set True or False
+MULTIPLE_DATABASE = bool(environ.get('MULTIPLE_DATABASE', True)) # Set True or False
 
 # If Multiple Database Is True Then Fill All Three Below Database Uri Else You Will Get Error.
-O_DB_URI = environ.get('O_DB_URI', "mongodb+srv://nehal969797:nehalsingh969797@cluster0.deg4yto.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")   # This Dba Store
-F_DB_URI = environ.get('F_DB_URI', "mongodb+srv://nehal969797:nehalsingh969797@cluster0.ngljy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")   # This Db Is For File Data Store
-S_DB_URI = environ.get('S_DB_URI', "mongodb+srv://Jinwoo2:nehalsingh969797@cluster0.ngxo2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")   # This Db is for File Data Store When First Db Is Going To Be Full.
+O_DB_URI = environ.get('O_DB_URI', "mongodb+srv://nehal969797:nehalsingh969797@cluster0.uojr9sg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")   # This Dba Store
+F_DB_URI = environ.get('F_DB_URI', "mongodb+srv://nehal969797:nehalsingh969797@cluster0.deg4yto.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")   # This Db Is For File Data Store
+S_DB_URI = environ.get('S_DB_URI', "mongodb+srv://nehal969797:nehalsingh969797@cluster0.ngljy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")   # This Db is for File Data Store When First Db Is Going To Be Full.
 
 
 # Premium And Referal Settings
